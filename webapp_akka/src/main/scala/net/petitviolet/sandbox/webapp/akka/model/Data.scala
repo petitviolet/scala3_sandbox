75c27d71d15416590af77c90a6ac159a12cd50c9
petitviolet <violethero0820@gmail.com>
2021-08-31 21:08:41 +0900

    implementing Data

---
+++ webapp_akka/src/main/scala/net/petitviolet/sandbox/webapp/akka/model/Data.scala
@@ -0,0 +1,80 @@
+package net.petitviolet.sandbox.webapp.akka.model
+
+import java.time.ZonedDateTime
+import java.util.UUID
+import scala.concurrent.ExecutionContext
+import scala.annotation.targetName
+
+case class Database(
+  id: DatabaseId,
+  name: DatabaseName,
+  updatedAt: ZonedDateTime,
+)
+opaque type DatabaseId = String
+object DatabaseId:
+  def apply(id: String): DatabaseId = id
+  def generate = apply(UUID.randomUUID().toString)
+
+extension (a: DatabaseId) @targetName("a_DatabaseId") def value: String = a
+
+opaque type DatabaseName = String
+object DatabaseName:
+  def apply(name: String): DatabaseName = name
+
+extension (a: DatabaseName) @targetName("a_DatabaseName") def value: String = a
+
+object Database:
+  def create(name: DatabaseName): Database =
+    apply(DatabaseId.generate, name, ZonedDateTime.now())
+
+case class Table(
+  id: TableId,
+  databaseId: DatabaseId,
+  name: TableName,
+  updatedAt: ZonedDateTime,
+)
+
+opaque type TableId = String
+object TableId:
+  def apply(id: String): TableId = id
+  def generate = apply(UUID.randomUUID().toString)
+
+extension (a: TableId) @targetName("a_TableId") def value: String = a
+
+opaque type TableName = String
+object TableName:
+  def apply(name: String): TableName = name
+
+extension (a: TableName) @targetName("a_TableName") def value: String = a
+
+case class Column(
+  id: ColumnId,
+  tableId: TableId,
+  name: ColumnName,
+  columnType: ColumnType,
+)
+
+opaque type ColumnId = String
+object ColumnId:
+  def apply(id: String): ColumnId = id
+  def generate = apply(UUID.randomUUID().toString)
+
+opaque type ColumnName = String
+object ColumnName:
+  def apply(name: String): ColumnName = name
+
+enum ColumnType {
+  case Int, Long, Varchar
+}
+
+type Async = [T] =>> (ExecutionContext ?=> T)
+
+trait DatabaseStore:
+  def findById(databaseId: DatabaseId): Async[Option[Database]]
+
+trait TableStore:
+  def findById(tableId: TableId): Async[Option[Table]]
+  def findAllByDatabaseId(databaseId: DatabaseId): Async[Seq[Table]]
+
+trait ColumnStore:
+  def findAllByTableId(tableId: TableId): Async[Seq[Column]]
