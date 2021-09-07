8e742afb393010f3cf2bf6cab7f7149c9251b2ea
petitviolet <violethero0820@gmail.com>
2021-09-07 21:15:06 +0900

    Automatic JsonMarshaller

--- webapp_akka/src/main/scala/net/petitviolet/sandbox/webapp/akka/CirceSupport.scala
+++ webapp_akka/src/main/scala/net/petitviolet/sandbox/webapp/akka/CirceSupport.scala
@@ -1,9 +1,10 @@
 package net.petitviolet.sandbox.webapp.akka
 
-import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, MediaTypes}
 import akka.http.scaladsl.unmarshalling.Unmarshaller
 import akka.util.ByteString
-import io.circe.{jawn, Decoder}
+import io.circe.{jawn, Decoder, Encoder, Printer}
 
 import scala.deriving.Mirror
 
@@ -24,4 +25,24 @@ trait CirceSupport {
   given [T](using inline A: Mirror.Of[T]): Decoder[T] =
     io.circe.generic.semiauto.deriveDecoder(using A)
 
+  type JsonMarshaller = [T] =>> (Encoder[T] ?=> ToEntityMarshaller[T])
+
+  given [T]: JsonMarshaller[T] = {
+    val contentType = ContentTypes.`application/json`
+    Marshaller.withFixedContentType(contentType) { t =>
+      HttpEntity(
+        contentType,
+        ByteString(
+          Printer.noSpaces.printToByteBuffer(
+            summon[Encoder[T]].apply(t),
+            contentType.charset.nioCharset(),
+          ),
+        ),
+      )
+    }
+  }
+
+  inline
+  given [T](using inline A: Mirror.Of[T]): Encoder[T] =
+    io.circe.generic.semiauto.deriveEncoder(using A)
 }
